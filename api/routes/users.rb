e47a9b268d74579f401518fbe18705b29ebc45b3
John Park <park_john@bah.com>
2015-04-11 12:28:03 -0400

    initial commit for routes

---
+++ api/routes/users.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'mongoid'
+require_relative '../models/users'
+
+module Sinatra
+  module SampleApp
+    module Routing
+      module Users
+        def self.registered(app)
+
+          get_users = lambda do
+            User.where(user_name: params['userName'])
+          end
+          app.get '/users', &get_users
+
+          post_user = lambda do
+            user = Users.new
+            user.user_name = params['userName']
+            user.name = params['name']
+            user.clan = params['clan']
+            user.insert
+          end
+          app.post '/user', &post_user
+        end
+      end
+    end
+  end
+end
