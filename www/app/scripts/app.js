a149bddb51c6360440e7bbf84e6aa5c09f71839a
Eric Fields <fields_eric@bah.com>
2015-04-11 13:34:22 -0400

    Added map controller/view with angular mapbox; changed grunt port to 8080

--- www/app/scripts/app.js
+++ www/app/scripts/app.js
@@ -15,19 +15,24 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'angular-mapbox'
   ])
+  .constant('RIDB_API', {
+    HOST: 'https://ridb.recreation.gov/api/v1',
+    KEY: '718A093253714004B839308AE31E7AD8'
+  })
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+      .when('/map', {
+        templateUrl: 'views/map.html',
+        controller: 'MapCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/map'
       });
   });
--- www/test/karma.conf.js
+++ www/test/karma.conf.js
@@ -28,6 +28,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-mapbox/dist/angular-mapbox.js',
+      'bower_components/mapbox.js/mapbox.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
