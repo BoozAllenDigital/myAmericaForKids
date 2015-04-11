bd5c8f1eeccbb71ca3171a849a1400f7a87588d0
yyw <(null)>
2015-04-11 12:02:00 -0400

    -Added username to users model

--- api/models/users.rb
+++ api/models/users.rb
@@ -5,6 +5,7 @@ class Users
 
   store_in collection: 'Users', database: 'mydb', session: 'default'
 
+  field :'user_name', as: 'user_name', type: String
   field :'name', as: :'name', type: String
   field :'clan', as: :'clan', type: String
 end
