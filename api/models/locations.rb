77232ddffac9c48d7a13a6f8cf851e455a6acd08
yyw <(null)>
2015-04-11 15:56:28 -0400

    -Added validation for Locations based on 24 hours.

--- api/models/locations.rb
+++ api/models/locations.rb
@@ -9,7 +9,7 @@ class Locations
   field :'latitude', as: :latitude, type: String
   field :'longitude', as: :longtidue, type: String
   field :'recAreaId', as: :recarea_id, type: String
-  field :'timestamp', as: :timestamp, type: Date
+  field :'timestamp', as: :timestamp, type: Time
   field :'score', as: :score, type: Integer
   field :'fromCamera', as: :from_camera, type: Boolean
 
