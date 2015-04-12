84307c371f4a89ddee65c12a921c3e7f0283ee21
Eric Fields <fields_eric@bah.com>
2015-04-11 23:58:21 -0400

    Removed unneeded files

--- www/app/scripts/controllers/knowledge.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('oNetApp')
-  .controller('KnowledgeCtrl', function ($scope, $location) {
-
-  });
--- www/app/scripts/controllers/skills.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('oNetApp')
-  .controller('SkillsCtrl', function ($scope, $location) {
-
-  });
